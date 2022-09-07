aaf21dc9e89824699165c94b0fafed7e08755c41
Sitepu <dsitepu@gfoundries.com>
2022-09-07 20:15:05 +0800

    Able to perform API for put and get

---
+++ main.py
@@ -0,0 +1,42 @@
+
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=str, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=str, help="Likes of the video", required=True)
+
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+api.add_resource(Video, "/video/<int:video_id>")
+
+
+# names = {"tim": {"age": 19, "gender": "male"},
+#         "bill" : {"age": 70, "gender" : "male"}}
+
+# class HelloWorld(Resource):
+#     def get(self, name):
+#         return names[name]
+
+# We are registering the class HelloWorld from above and if user goes to /helloworld it will call the get request
+# api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+
+if __name__ == "__main__":
+    # please comment below if run in production environment
+    app.run(debug=True)
+
---
+++ test.py
@@ -0,0 +1,13 @@
+import requests 
+
+BASE = "http://127.0.0.1:5000/"
+
+# This means I want to get request from the base + helloworld
+# response = requests.get(BASE + "helloworld/tim")
+response = requests.put(BASE + "video/1", {"likes": 10, "name": "Tim", "views": 100000})
+print(response.json())
+
+input()
+
+response = requests.get(BASE + "video/1")
+print(response.json())
