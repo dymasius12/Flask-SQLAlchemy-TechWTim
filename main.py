682ff6d25e5a27b9fd1edb798b427b8cc11e0967
Sitepu <dsitepu@gfoundries.com>
2022-09-07 21:51:25 +0800

    debugged one problem 57:50, found an error TypeError: get() got an unexpected keyword argument 'id'

--- main.py
+++ main.py
@@ -1,36 +1,69 @@
 
 
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+# Creating the model of our database
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
+
+# After you run once with the db.create_all please comment so you dont keep populating the database
+# db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
 video_put_args.add_argument("views", type=str, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=str, help="Likes of the video", required=True)
 
-videos = {}
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
-def abort_if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message="Could not find the video..")
+# videos = {}
 
-def abort_if_video_exists(video_id):
-    if video_id in videos:
-        abort(404, message="Video already exist with that id..")
+# def abort_if_video_id_doesnt_exist(video_id):
+#     if video_id not in videos:
+#         abort(404, message="Could not find the video..")
+
+# def abort_if_video_exists(video_id):
+#     if video_id in videos:
+#         abort(404, message="Video already exist with that id..")
 
 class Video(Resource):
+    # Basically return the resource field as json
+    @marshal_with(resource_fields) 
     def get(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        return videos[video_id]
+        # abort_if_video_id_doesnt_exist(video_id)
+        result = VideoModel.query.get(id=video_id)
+        # return videos[video_id]
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abort_if_video_exists(video_id)
+        # abort_if_video_exists(video_id)
+        # args = video_put_args.parse_args()
+        # videos[video_id] = args
+        # return videos[video_id], 201
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
 
     def delete(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
