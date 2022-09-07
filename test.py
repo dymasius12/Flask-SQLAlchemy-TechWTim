0667b84a4b70be66973c1b92e105a2b6ed0b16d9
Sitepu <dsitepu@gfoundries.com>
2022-09-07 20:32:59 +0800

    able to do delete request and check whether exist or not

--- test.py
+++ test.py
@@ -4,10 +4,19 @@ BASE = "http://127.0.0.1:5000/"
 
 # This means I want to get request from the base + helloworld
 # response = requests.get(BASE + "helloworld/tim")
-# response = requests.put(BASE + "video/1", {"likes": 10, "name": "Tim", "views": 100000})
-# print(response.json())
 
-# input()
+data = [{"likes": 78, "name": "Joe", "views": 100000},
+        {"likes": 2000000, "name": "How to make REST API", "views": 500000000},
+        {"likes": 35, "name": "Tim", "views": 100000}]
 
-response = requests.get(BASE + "video/6")
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+
+response = requests.get(BASE + "video/2")
 print(response.json())
